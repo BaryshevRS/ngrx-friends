74317e45a1a745735aca6d1c18e0ae78f167c81b
baryshevrs <baryshevrs@yandex.ru>
2019-02-11 13:50:58 +0300

    Правка рейтинга

--- src/app/component/rating/rating.component.ts
+++ src/app/component/rating/rating.component.ts
@@ -1,22 +1,38 @@
-import {Input, Component, Output, EventEmitter} from '@angular/core';
+import {Input, Component, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-rating',
     templateUrl: './rating.component.html',
     styleUrls: ['./rating.component.scss']
 })
-export class RatingComponent {
-
-    public ratingValue: number[] = [1, 2, 3, 4, 5];
+export class RatingComponent implements OnInit {
 
     @Output() ratingChange: EventEmitter<number> = new EventEmitter();
 
     @Input() id: string;
     @Input() value: number;
+    @Input() ratingSize: number;
+
+    private defaultRatingSize = 5;
+    public ratingSizeView: number[];
 
     public setValue(rating: number): void {
         this.value = rating;
         this.ratingChange.emit(rating);
     }
 
+    private setRatingSize() {
+        const ratingSize = this.ratingSize || this.defaultRatingSize;
+        let ratingSizeView: number[] = [];
+
+        for(let i = 1; i <= ratingSize; i++) {
+            ratingSizeView.push(i);
+        }
+
+        this.ratingSizeView = ratingSizeView;
+    }
+
+    ngOnInit(): void {
+        this.setRatingSize();
+    }
 }

50084ca08117434cd439349e9cc80779d8227c4c
baryshevrs <baryshevrs@yandex.ru>
2019-04-11 16:03:36 +0300

    add more test for friends and local store service

--- src/app/service/friends.service.spec.ts
+++ src/app/service/friends.service.spec.ts
@@ -1,18 +1,91 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
 import {FriendsService} from './friends.service';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {Friend} from '../class/friends';
+import {of} from 'rxjs';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {LocalSaveService} from './local-save.service';
 
 describe('FriendsService', () => {
+
+    let moskFriends;
+    let moskConfigsFriends;
+    let friendsService: FriendsService;
+    let httpClientSpy: { get: jasmine.Spy };
+
+    moskConfigsFriends = {typeSort: 0, searchValue: '', showBookmark: false, startView: 0, limitView: 10};
+    moskFriends = [new Friend('1', 'first_name', 'last_name')];
+
     beforeEach(() => {
+
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+        const LocalSaveMosk = {
+            get: () => {},
+            set: () => {}
+        };
+
         TestBed.configureTestingModule({
-            providers: [FriendsService],
+            providers: [
+                FriendsService,
+                {provide: HttpClient, useValue: httpClientSpy},
+                {provide: LocalSaveService, useValue: LocalSaveMosk}
+            ],
             imports: [
-                HttpClientModule
+                HttpClientTestingModule
             ],
         });
+
+        friendsService = TestBed.get(FriendsService);
     });
 
     it('should be created', inject([FriendsService], (service: FriendsService) => {
         expect(service).toBeTruthy();
     }));
+
+    it('should be get friends', async(() => {
+
+        httpClientSpy.get.and.returnValue(of(moskFriends));
+
+        friendsService.getFriends(moskConfigsFriends).subscribe(friends => {
+            expect({...friends}).toEqual({...moskFriends});
+        }, fail);
+
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'called once');
+    }));
+
+    it('should be get friend by id', async(() => {
+
+        httpClientSpy.get.and.returnValue(of(moskFriends));
+
+        friendsService.getFriend('1').subscribe(friend => {
+            expect({...friend}).toEqual({...moskFriends[0]});
+        }, fail);
+
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'called once');
+    }));
+
+    it('should be filter bookmark', inject([FriendsService], (service: FriendsService) => {
+
+        const serviceMoskFriends = [new Friend('1', 'first_name', 'last_name')];
+        serviceMoskFriends[0].bookmark = 1;
+
+        expect({...service.getFilterBookmark(serviceMoskFriends)}).toEqual({...serviceMoskFriends});
+
+    }));
+
+    it('should be get count bookmarsk friends', async(() => {
+
+        const serviceMoskFriends = [new Friend('1', 'first_name', 'last_name')];
+        serviceMoskFriends[0].bookmark = 1;
+
+        httpClientSpy.get.and.returnValue(of(serviceMoskFriends));
+
+        friendsService.getCountBookmarskFriends().subscribe(count => {
+            expect(count).toBe(1);
+        }, fail);
+
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'called once');
+    }));
+
 });
--- src/app/service/local-save.service.spec.ts
+++ src/app/service/local-save.service.spec.ts
@@ -3,16 +3,96 @@ import {HttpClientModule} from '@angular/common/http';
 import {LocalSaveService} from './local-save.service';
 
 describe('LocalSaveService', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [LocalSaveService],
-            imports: [
-                HttpClientModule
-            ],
+
+    describe('Localstorage empty', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                providers: [LocalSaveService],
+                imports: [
+                    HttpClientModule
+                ],
+            });
+
+            const store = {};
+
+            spyOn(localStorage, 'getItem').and.callFake(function (key: string): string {
+                return store[key] || '';
+            });
+
+            spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+                return store[key] = value.toString();
+            });
+
         });
+
+        it('should be created', inject([LocalSaveService], (service: LocalSaveService) => {
+            expect(service).toBeTruthy();
+        }));
+
+        it('should be set', inject([LocalSaveService], (service: LocalSaveService) => {
+            const moskName = 'friendsBookmark';
+            const moskValue = {'id1': 1};
+
+            service.set(moskName, moskValue);
+
+            expect(localStorage.getItem).toHaveBeenCalled();
+            expect(localStorage.getItem).toHaveBeenCalledWith(moskName);
+            expect(localStorage.setItem).toHaveBeenCalledWith(moskName, JSON.stringify(moskValue));
+        }));
+
+        it('should be get', inject([LocalSaveService], (service: LocalSaveService) => {
+            const moskName = 'friendsBookmark';
+
+            expect(service.get(moskName)).toBeNull();
+
+            expect(localStorage.getItem).toHaveBeenCalled();
+            expect(localStorage.getItem).toHaveBeenCalledWith(moskName);
+        }));
+
     });
 
-    it('should be created', inject([LocalSaveService], (service: LocalSaveService) => {
-        expect(service).toBeTruthy();
-    }));
+    describe('Localstorage full', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                providers: [LocalSaveService],
+                imports: [
+                    HttpClientModule
+                ],
+            });
+
+            const store = {'friendsBookmark' : '{"id1":1}'};
+
+            spyOn(localStorage, 'getItem').and.callFake(function (key: string): string {
+                return store[key] || undefined;
+            });
+
+            spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+                return store[key] = value.toString();
+            });
+
+        });
+
+        it('should be created', inject([LocalSaveService], (service: LocalSaveService) => {
+            expect(service).toBeTruthy();
+        }));
+
+        it('should be set', inject([LocalSaveService], (service: LocalSaveService) => {
+            const moskName = 'friendsBookmark';
+            const moskValue = {'id2': 1};
+            const moskValueReturn = {'id1': 1, 'id2' : 1};
+
+            service.set(moskName, moskValue);
+
+            expect(localStorage.setItem).toHaveBeenCalledWith(moskName, JSON.stringify(moskValueReturn));
+        }));
+
+        it('should be get', inject([LocalSaveService], (service: LocalSaveService) => {
+            const moskName = 'friendsBookmark';
+            const moskValue = {'id1': 1};
+
+            expect({...service.get(moskName)}).toEqual({...moskValue});
+            expect(localStorage.getItem).toHaveBeenCalledWith(moskName);
+        }));
+
+    });
 });

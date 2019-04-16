c32af0684f67f3cf938f6e9389561f7943482073
baryshevrs <baryshevrs@yandex.ru>
2019-04-16 13:23:16 +0300

    add test for scroll directive

--- src/app/directive/infinity-scroll-loader.directive.spec.ts
+++ src/app/directive/infinity-scroll-loader.directive.spec.ts
@@ -2,86 +2,168 @@ import {InfinityScrollLoaderDirective} from './infinity-scroll-loader.directive'
 import {DOCUMENT} from '@angular/common';
 import {Friend} from '../class/friends';
 import {Component, DebugElement, ViewChild} from '@angular/core';
-import {ComponentFixture, TestBed, async} from '@angular/core/testing';
+import {ComponentFixture, TestBed, async, fakeAsync, tick} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
-
-const moskFriends: Friend[] = [
-    new Friend('1', 'first_name', 'last_name'),
-    new Friend('2', 'first_name', 'last_name'),
-    new Friend('3', 'first_name', 'last_name'),
-    new Friend('4', 'first_name', 'last_name'),
-    new Friend('5', 'first_name', 'last_name'),
-    new Friend('6', 'first_name', 'last_name'),
-    new Friend('7', 'first_name', 'last_name'),
-    new Friend('8', 'first_name', 'last_name'),
-    new Friend('9', 'first_name', 'last_name'),
-    new Friend('10', 'first_name', 'last_name'),
-    new Friend('11', 'first_name', 'last_name'),
-    new Friend('12', 'first_name', 'last_name')
-];
-
-@Component({
-    template: `
-      <div
-           appInfinityScrollLoader
-           [contents]="contents"
-           (drawing)="drawing($event)">
-      </div>
-    `
-})
-class TestComponent {
-
-    public contents = moskFriends;
-
-    @ViewChild('InfinityScrollLoaderDirective') directive: InfinityScrollLoaderDirective;
-
-    drawing($event) {
-    }
-}
 
 describe('InfinityScrollLoaderDirective', () => {
 
-    let fixture: ComponentFixture<TestComponent>;
-    let des: DebugElement;
-    let directive;
+    describe('On scroll', () => {
 
-    beforeEach(async(() => {
-        fixture = TestBed.configureTestingModule({
-            declarations: [InfinityScrollLoaderDirective, TestComponent],
-        }).createComponent(TestComponent);
+        const moskFriends: Friend[] = [
+            new Friend('1', 'first_name', 'last_name'),
+            new Friend('2', 'first_name', 'last_name'),
+            new Friend('3', 'first_name', 'last_name'),
+            new Friend('4', 'first_name', 'last_name'),
+            new Friend('5', 'first_name', 'last_name'),
+            new Friend('6', 'first_name', 'last_name'),
+            new Friend('7', 'first_name', 'last_name'),
+            new Friend('8', 'first_name', 'last_name')
+        ];
 
-        des = fixture.debugElement.query(By.directive(InfinityScrollLoaderDirective));
-        directive = new InfinityScrollLoaderDirective(fixture, DOCUMENT);
-        fixture.detectChanges();
+        @Component({
+            template: `
+              <div
+                  appInfinityScrollLoader
+                  [contents]="contents"
+                  (drawing)="drawing($event)">
+                <div class="row"
+                     *ngFor="let friend of contents;">
+                </div>
+              </div>
+            `,
+            styles: [`
+                .row {
+                    width: 100px;
+                    height: 100px;
+                }
+            `]
+        })
+        class TestComponent {
 
+            public contents = moskFriends;
 
-    }));
+            @ViewChild(InfinityScrollLoaderDirective) directive: InfinityScrollLoaderDirective;
 
-    afterEach(() => {
-        fixture = null;
+            drawing($event) {
+            }
+        }
+
+        let component: TestComponent;
+        let fixture: ComponentFixture<TestComponent>;
+        let des: DebugElement;
+        let directive;
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                declarations: [TestComponent, InfinityScrollLoaderDirective]
+            }).compileComponents();
+        }));
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(TestComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+            des = fixture.debugElement.query(By.directive(InfinityScrollLoaderDirective));
+        });
+
+        afterEach(() => {
+            fixture = null;
+        });
+
+        it('should create an instance', () => {
+            directive = new InfinityScrollLoaderDirective(fixture, DOCUMENT);
+            expect(directive).toBeTruthy();
+        });
+
+        it('should create drawing', fakeAsync(() => {
+
+            const drawingSpy = spyOn(component, 'drawing');
+
+            window.scrollTo(0, 100);
+            window.dispatchEvent(new Event('scroll'));
+            tick(300);
+
+            fixture.detectChanges();
+
+            window.scrollTo(0, 200);
+            window.dispatchEvent(new Event('scroll'));
+            tick(300);
+
+            expect(drawingSpy).toHaveBeenCalled();
+
+        }));
     });
 
-    it('should create an instance', () => {
-        expect(directive).toBeTruthy();
+    describe('fill on load', () => {
+
+        const moskFriends: Friend[] = [
+            new Friend('1', 'first_name', 'last_name'),
+            new Friend('2', 'first_name', 'last_name'),
+            new Friend('3', 'first_name', 'last_name'),
+            new Friend('4', 'first_name', 'last_name')
+        ];
+
+        @Component({
+            template: `
+              <div
+                  appInfinityScrollLoader
+                  [contents]="contents"
+                  (drawing)="drawing($event)">
+                <div class="row"
+                     *ngFor="let friend of contents;">
+                </div>
+              </div>
+            `,
+            styles: [`
+                .row {
+                    width: 100%;
+                    height: 100px;
+                }
+            `]
+        })
+        class TestComponent {
+
+            public contents = moskFriends;
+
+            @ViewChild(InfinityScrollLoaderDirective) directive: InfinityScrollLoaderDirective;
+
+            drawing($event) {
+                console.log('drawing2');
+            }
+        }
+
+        let component: TestComponent;
+        let fixture: ComponentFixture<TestComponent>;
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                declarations: [TestComponent, InfinityScrollLoaderDirective]
+            }).compileComponents();
+        }));
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(TestComponent);
+            component = fixture.componentInstance;
+
+        });
+
+        afterEach(() => {
+            fixture = null;
+        });
+
+        it('should create drawing',   (  async () => {
+
+            fixture.whenStable().then(() => {
+                const drawingSpy = spyOn(component, 'drawing');
+                fixture.detectChanges();
+                expect(drawingSpy).toHaveBeenCalled();
+
+                const ngAfterViewCheckedSpy = spyOn(component.directive, 'ngAfterViewChecked');
+                fixture.detectChanges();
+                expect(ngAfterViewCheckedSpy).toHaveBeenCalled();
+            });
+
+        }));
+
     });
-
-    it('should create an instance 2', async() => {
-        fixture.detectChanges();
-
-        // const spy = spyOn(directive, 'ngAfterViewInit');
-        // expect(spy).toHaveBeenCalled();
-    });
-
-    // dispatchSpy = spyOn(store, 'dispatch');
-    // const componentDebug = fixture.debugElement;
-    // const slider = componentDebug.queryAll(By.directive(MatSlideToggle))[2];
-    //
-    // slider.triggerEventHandler('change', { checked: false });
-    // fixture.detectChanges();
-    //
-    // expect(dispatchSpy).toHaveBeenCalledTimes(1);
-    // expect(dispatchSpy).toHaveBeenCalledWith(
-    //     new ActionSettingsChangeAnimationsPage({ pageAnimations: false })
-    // );
-
 });

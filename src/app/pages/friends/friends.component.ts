6883eaeb8af1b6a54cb9e4d8fdd6b6c866f600de
BaryshevRS <baryshevrs@yandex.ru>
2022-01-21 17:04:48 +0300

    Loading rendered to fix

--- src/app/pages/friends/friends.component.ts
+++ src/app/pages/friends/friends.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { getErrors, getFriends, getLoading } from '../../store/selector/friends.selector';
 import * as FriendsActions from '../../store/action';
 import { FriendsState } from './shared/interfaces';
+import { delay } from 'rxjs/internal/operators/delay';
 
 @Component({
   selector: 'app-friends',
@@ -13,7 +14,7 @@ import { FriendsState } from './shared/interfaces';
 export class FriendsComponent implements OnInit {
   public friends$: Observable<Friend[]> = this.store$.pipe(select(getFriends));
   public errors$ = this.store$.pipe(select(getErrors));
-  public loading$ = this.store$.pipe(select(getLoading));
+  public loading$ = this.store$.pipe(delay(0), select(getLoading));
 
   constructor(private store$: Store<FriendsState>) {}
 
